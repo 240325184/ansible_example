8a2c7a2e8406110d5c22cdaadcd12ed70cfac1e9
mgd <mgd@iZ23ll0ggxjZ.(none)>
2016-02-23 23:48:40 +0800

    d

--- mysql_master_install/roles/mysql_master_install/files/mysql/basedir/sql-bench/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /data/soft/mysql/Percona-Server-5.5.21-rel25.1/sql-bench
-# Build directory: /data/soft/mysql/Percona-Server-5.5.21-rel25.1/sql-bench
-# 
-# This file replicates the SUBDIRS() and ADD_TEST() commands from the source
-# tree CMakeLists.txt file, skipping any SUBDIRS() or ADD_TEST() commands
-# that are excluded by CMake control structures, i.e. IF() commands.
